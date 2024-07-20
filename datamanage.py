bcb37a8ae7b045586502e24ec35df8b4296b82c6
alireza <alireza@ubuntu.com>
2024-07-20 19:47:33 +0330

    Edits to restructure the db storage system

--- datamanage.py
+++ datamanage.py
@@ -1,2 +1,24 @@
+
+# Creating data managing tools for ease of access
+
+import datetime
+import sqlite3
+
+
 class Data:
-    pass
+    def __init__(self):
+        pass
+
+    def addDay(self,timex =datetime.datetime.now()):
+        
+        con = sqlite3.connect('data.db')
+        cur = con.cursor()
+
+        cmd1 = f'CREATE TABLE rec_{timex.year}_{timex.month}_{timex.day} (starttime, endtime, activity, tag, priority)'
+        cur.execute(cmd1)
+            
+
+
+        con.commit()
+        con.close()
+
