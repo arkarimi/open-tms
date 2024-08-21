37c208cb0c9dfc3a964367e34e5cc850e7cb7b20
alireza <alireza@ubuntu.com>
2024-08-21 21:45:53 +0330

    Adding datareader function to the file

--- datamanage.py
+++ datamanage.py
@@ -9,6 +9,64 @@ class Data:
     def __init__(self):
         pass
 
+    
+    def todayTableName(self):
+        timex =datetime.datetime.now()
+        today = f'rec_{timex.year}_{timex.month}_{timex.day}'
+
+        return today
+
+    def checktodayexists(self):
+        timex =datetime.datetime.now()
+        exists = 0
+        today = f'rec_{timex.year}_{timex.month}_{timex.day}'
+
+        con = sqlite3.connect('data.db')
+        cur = con.cursor()
+
+        cmd1 = f"SELECT name FROM sqlite_master WHERE type='table' ORDER BY name"
+        cur.execute(cmd1)
+
+        tables = cur.fetchall()
+
+        con.commit()
+        con.close()
+
+        for table in tables:
+            if table[0] == today:
+                exists = 1
+
+
+
+
+
+
+
+        return exists
+    
+    def checkotherdayexists(recordname):
+        
+        exists = 0
+        today = recordname
+
+        con = sqlite3.connect('data.db')
+        cur = con.cursor()
+
+        cmd1 = f"SELECT name FROM sqlite_master WHERE type='table' ORDER BY name"
+        cur.execute(cmd1)
+
+        tables = cur.fetchall()
+
+        con.commit()
+        con.close()
+
+        for table in tables:
+            if table[0] == today:
+                exists = 1
+
+
+        return exists
+
     def addDay(self,timex =datetime.datetime.now()):
         
         con = sqlite3.connect('data.db')
@@ -22,3 +80,28 @@ class Data:
         con.commit()
         con.close()
 
+    def addtable(self, name):
+        con = sqlite3.connect('data.db')
+        cur = con.cursor()
+
+        cmd1 = f'CREATE TABLE {name} (starttime, endtime, activity, tag, priority)'
+        cur.execute(cmd1)
+            
+
+
+        con.commit()
+        con.close()
+
+    def getdaydata(self, date):
+        conn = sqlite3.connect('data.db')
+        cursor = conn.cursor()
+
+        cursor.execute(f'SELECT * FROM {date}')
+        daydata = cursor.fetchall()
+        conn.close()
+
+        return daydata
+
+
+d = Data()
+d.checktodayexists()
